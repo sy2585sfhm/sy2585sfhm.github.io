2243bbfc2d1403f85e8a5f4e7196a8be393cceed
sy2585sfhm <1510078226@qq.com>
2022-09-06 13:40:18 +0800

    修改了音乐播放器

--- 音乐播放器/music.js
+++ 音乐播放器/music.js
@@ -6,17 +6,17 @@ $(document).ready(function () {
     var music = [{
         title: '小芳',
         singer: '李荣浩',
-        mp3: 'http://ws.stream.qqmusic.qq.com/C400004Aqajj3rTOjU.m4a?guid=544839445&vkey=E4B7E8B118011599D9A93458581E818B5747928E5D9C8D412278A2F5225947FCD10E8264D414F8ED6884EF8297B95752A384A5E8298B3000&uin=&fromtag=120032',
+        mp3: './C400004Aqajj3rTOjU.m4a',
         poster: "http://33.media.tumblr.com/0b35eb42176eedbf4a96e52efa760875/tumblr_mxp7a0v3fr1rqx86wo1_500.png"
     }, {
         title: '你不是第一个离开的人',
         singer: '邓紫棋',
-        mp3: 'http://ws.stream.qqmusic.qq.com/C400002gIrZ419ssQG.m4a?guid=835336001&vkey=D3CA4659195151C69C3D3211AAF4EEC18328A3D2E043DEA2388BE89953E7003A06C3FFB6E85E53347A37F4A233B0ACB37EDA8790A0A4EC3E&uin=&fromtag=120032',
+        mp3: './C400002gIrZ419ssQG.m4a',
         poster: "http://33.media.tumblr.com/bf9dc125a47dcca91ce5b3575bc3ba92/tumblr_nbmb3j8nU51sq3g2zo1_500.png"
     }, {
         title: '如愿',
         singer: '王菲',
-        mp3: 'http://ws.stream.qqmusic.qq.com/C400001ufyHx10iWpg.m4a?guid=560245395&vkey=9AE446342D5C87FFE2A90EC073875B5770F75F53D785837DA10BD62155F10C005660BEEDF4802369DD153E43DAAAF6384B492619309486CC&uin=&fromtag=120032',
+        mp3: './C400001ufyHx10iWpg.m4a',
         poster: "http://31.media.tumblr.com/810b1125a8b9e9f192d009ef58dceb07/tumblr_nbe8wsmKuz1rknpqyo1_500.jpg"
     }]
     audio.src = music[0].mp3
